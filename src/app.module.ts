8d57c3d904c2f35756bc5a0653f74f2ac7425c3f
jonghyeon park <whdgus003@gmail.com>
2022-10-11 05:23:29 +0000

    podcast crud draft

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EpisodesModule } from './episodes/episodes.module';
+import { PodcastsModule } from './podcasts/podcasts.module';
 
 @Module({
-  imports: [],
+  imports: [PodcastsModule, EpisodesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
